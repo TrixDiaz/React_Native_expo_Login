d44326395f67112ed14b286f4a6b250d22a7a9ea
XirTech <john.darlucio022@gmail.com>
2023-04-22 14:49:49 +0800

    add design on register

--- screens/Register.js
+++ screens/Register.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
-import { StyleSheet, TextInput, View, Button } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  Image,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import FlatButton from "../shared/buttons/button";
+import laptop from "../assets/laptop.jpg"
 
  function RegistrationForm() {
   const [username, setUsername] = useState("");
@@ -17,28 +26,32 @@ import FlatButton from "../shared/buttons/button";
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={setPassword}
-      />
-      <FlatButton text="Register" onPress={handleRegister} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image source={laptop} style={styles.laptop}></Image> 
+      <View>
+        <Text style={styles.title}>REGISTER</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
+        <FlatButton text="Register" onPress={handleRegister} />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -48,17 +61,29 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
+    backgroundColor: "#FAF9FF",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#000",
   },
   input: {
     height: 40,
-    width: "100%",
+    width: 320,
     marginVertical: 10,
     borderWidth: 1,
-    borderColor: "#ccc",
+    borderColor: "#000",
     paddingHorizontal: 10,
+    borderRadius: 10,
   },
   button: {
     marginTop: 10,
+  },
+  laptop: {
+    width: 300,
+    height: 150,
   },
 });
 
